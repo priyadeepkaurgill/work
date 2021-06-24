6240639b21147358582d8029ac2a823580fc69e1
priyadeepkaurgill <86386083+priyadeepkaurgill@users.noreply.github.com>
2021-06-23 21:11:32 -0400

    Update interview.js

--- interview.js
+++ interview.js
@@ -1,6 +1,6 @@
 
 
-class MyForm extends React.Component {
+class MyForm extends React.Component {                          /*constructor*/
     constructor(props) {
         super(props);
         this.state = {
@@ -8,25 +8,25 @@ class MyForm extends React.Component {
             Password: '',
         };
     }
-    mySubmitHandler = (event) => {
+    mySubmitHandler = (event) => {                      /*function run on submit button*/
         event.preventDefault();
         let Email = this.state.Email;
-        if (!(Email)) {
-            alert("Please enter your email" + [Email]);
+        if (!(Email)) {                                 /* If Email textbox is empty*/
+            alert("Please enter your email");
         }
-        let Password = this.state.Password;
+        let Password = this.state.Password;            /*If Password textbox is empty*/
         if (!(Password)) {
             alert("Please enter your password");
         }
     }
 
-    myChangeHandler = (event) => {
+    myChangeHandler = (event) => {                  /* change of textboxes sets the value of state*/
         let nam = event.target.name;
         let val = event.target.value;
         this.setState({ [nam]: val });
     }
-    render() {
-        const mystyle = {
+    render() {                                  
+        const mystyle = {                         /* In file CSS style*/
             color: 'black',
             fontWeight: 'bold',
             textAlign: 'left',
@@ -34,18 +34,18 @@ class MyForm extends React.Component {
 
 
 
-        return (
+        return (                                                                                { /*html for the page*/}
             <form onSubmit={this.mySubmitHandler} method="POST" >
                 <h1>
                     Sign In </h1>
                 <p style={mystyle}>Email<br />
-                    <input
+                    <input                                                                         { /*email textbox*/}
                         type='text'
                         name='Email'
                         onChange={this.myChangeHandler}
                     />
                 </p>
-                <p style={mystyle}>Password<br />
+                <p style={mystyle}>Password<br />                                                {/*password textbox*/}
                     <input
                         type='text'
                         name='Password'
@@ -53,12 +53,11 @@ class MyForm extends React.Component {
                     />
                 </p>
 
-                <input type="checkbox" id="RememberMe" name="rem" />
+                <input type="checkbox" id="RememberMe" name="rem" />                           {/*remember me check box*/}
                 <label style={{ fontSize: '16px' }} id="Remember-me">Remember me?</label>
-
                 <br />
                 <br />
-                <button onClick={this.mySubmitHandler}>Sign in</button>
+                <button onClick={this.mySubmitHandler} > Sign in </button>                       
                 <br />
                 <p ><a href=" ">Forgot your password?</a></p>
                 <p>Don't have an account?<a href="">Sign up</a></p>
@@ -70,5 +69,5 @@ class MyForm extends React.Component {
     }
 }
 
-ReactDOM.render(<MyForm />, document.getElementById('root'));
-// JavaScript source code
+ReactDOM.render(<MyForm />, document.getElementById('root'));                      
+
